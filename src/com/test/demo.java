6c38b4db83f0b43f1fd5e787ee938f729377bedb
zyf1169013790 <72647894+zyf1169013790@users.noreply.github.com>
2021-05-15 21:33:44 +0800

    Update demo.java

--- src/com/test/demo.java
+++ src/com/test/demo.java
@@ -8,7 +8,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println(111);
         System.out.println(1112);
-        System.out.println(000);
+        System.out.println(00011);
         ArrayList a = new ArrayList();
 
     }
