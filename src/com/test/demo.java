d492452714d4371f008904f5ab6561ce4f5f4f3e
zyf1169013790 <2314048826@qq.com>
2021-05-15 21:21:21 +0800

    1

--- src/com/test/demo.java
+++ src/com/test/demo.java
@@ -8,7 +8,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println(111);
         System.out.println(1112);
-        System.out.println(1121);
+        System.out.println(000);
         ArrayList a = new ArrayList();
 
     }
