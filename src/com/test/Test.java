ccc16f0eb0aaf06bf8921f02630ef87441ed4af4
zyf1169013790 <2314048826@qq.com>
2021-05-15 21:16:23 +0800

    Initial commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author zyf
+ * @date 2021-05-14
+ */
+public class Test {
+    public static void main(String[] args) {
+        demo.test();
+    }
+
+}
---
+++ src/com/test/demo.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class demo {
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(1112);
+        System.out.println(1121);
+        ArrayList a = new ArrayList();
+
+    }
+
+    public static void test() {
+        System.out.println(111);
+    }
+
+}
